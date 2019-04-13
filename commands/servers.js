648626fabbb09c318d926036ddedfe4832dd7d73
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-13 20:25:57 +0200

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,1 +1,19 @@
+const { version } = require("discord.js");
+exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  message.channel.send(`= Servers/users =
+• Users      :: ${client.users.size.toLocaleString()}
+• Servers    :: ${client.guilds.names.toLocaleString()}
 
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "servers",
+  category: "Miscelaneous",
+  description: "Gives some useful server statistics",
+  usage: "servers"
+};
