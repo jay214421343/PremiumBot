a044264b0fe7c88293858c01d4d54fa6a41d949c
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-14 12:21:57 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
   
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const inviteLink = `https://discordapp.com/api/oauth2/authorize?client_id=561834611156582420&permissions=2080898295&redirect_uri=https%3A%2F%2Fdiscord.gg%2FcqMkUuc&scope=bot`;
-  message.channel.send(`To invite Etiket, click on the following link:\n<${inviteLink}>`);
+  message.channel.send(`To invite The Bot, click on the following link:\n<${inviteLink}>`);
 };
 
 exports.conf = {
